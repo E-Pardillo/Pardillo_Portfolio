207dbcd9c38f1212a66bec2c6bae3a1e0a0d9a5f
E-Pardillo <e.pardillo.515474@umindanao.edu.ph>
2024-08-29 01:29:02 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://www.wcofun.net/the-loud-house-season-6-episode-25-fashion-no-show-doom-service" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://umindanao.edu.ph/colleges/cce" target="_blank">
+          <img src="src/assets/CCE.png" class="object-scale-down h-20 w-20" />
+         
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+       eyyyy
+      </p>
+    </>
+  )
+}
+
+export default App
